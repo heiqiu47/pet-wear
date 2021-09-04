c4dbd9dca976a19795473bd0c5747d78523d7f5d
heiqiu47 <acscs1500@163.com>
2021-09-04 16:07:40 +0800

    Initial commit

---
+++ src/main/java/com/example/petwear/PetWearApplication.java
@@ -0,0 +1,13 @@
+package com.example.petwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetWearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetWearApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/petwear/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.example.petwear.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface UserMapper {
+    
+}
